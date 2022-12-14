e60da98b0db522453679c087775691b459b970ab
charismaticdog123 <agnes_tran@brown.edu>
2022-12-14 17:14:50 -0500

    trying hashrouter

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client';
 import "./style.css";
 import App from './App';
 
-import { BrowserRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <BrowserRouter basename="/finalportfolio">
+     <HashRouter basename="/finalportfolio">
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
